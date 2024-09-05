1d3ee809900c05d706b3d91711597b1823cc5c35
chenyilong369 <1456014766@qq.com>
2024-09-05 09:14:02 +0800

    feat: 提交 store 单元测试

--- tests/unit/store.spec.ts
+++ tests/unit/store.spec.ts
@@ -1,5 +1,9 @@
+import { TextComponentProps } from "@/defaultProps";
 import store from "@/store";
+import { testComponents } from "@/store/editor";
 import { testData } from "@/store/templates";
+import { clone } from "lodash";
+const cloneComponents = clone(testComponents)
 
 describe('test vuex', () => {
   it('should have three modules', () => {
@@ -26,6 +30,26 @@ describe('test vuex', () => {
     it('test getTemplateById is ok', () => {
       const selectTemplate = store.getters.getTemplateById(1)
       expect(selectTemplate.title).toBe('sdasd1')
+    })
+  })
+
+  describe('test editor module', () => {
+    it('should have default value', () => {
+      expect(store.state.editor.components).toHaveLength(testComponents.length)
+    })
+    it('should get current element when set active one component', () => {
+      const testId = testComponents[0].id
+      store.commit('setActive', testId)
+      expect(store.state.editor.currentElement).toBe(testId)
+      const currentElement = store.getters.getCurrentElement
+      expect(currentElement.id).toBe(testId)
+    })
+    it('add component should work fine', () => {
+      const payload: Partial<TextComponentProps> ={
+        text: 'text1'
+      }
+      store.commit('addComponent', payload)
+      expect(store.state.editor.components).toHaveLength(cloneComponents.length + 1)
     })
   })
 })
